3ab899727e7de44123da8ae61092aacac36173ac
Alex Zuniga <alexander.zuniga@fitmentgroup.com>
2024-07-29 21:45:52 -0500

    7.29 commit

--- budget_app/data_pipeline/load/sheets_handler.py
+++ budget_app/data_pipeline/load/sheets_handler.py
@@ -14,6 +14,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+from pandas import DataFrame
 import json
 
 # If modifying these scopes, delete the file token.json.
@@ -26,10 +27,7 @@ SAMPLE_RANGE_title = 'August-AZ!A:D'
 
 class GoogleHandler(object):
 
-    data_range = 'test'
-    user = 'AZ'
-    
-    def __init__(self, date_ranges, banking_data='PLACEHOLDER', user='AZ'):
+    def __init__(self, date_ranges=None, banking_data='PLACEHOLDER', user='AZ'):
         self.user = user
         self.date_ranges = date_ranges
         self.banking_data = banking_data
@@ -39,8 +37,7 @@ class GoogleHandler(object):
 
     def get_creds_and_build(self):
         creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first time.
+        # The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
         if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
         # If there are no (valid) credentials available, let the user log in.
@@ -56,23 +53,17 @@ class GoogleHandler(object):
                 token.write(creds.to_json())
         try:
             service = build('sheets', 'v4', credentials=creds)
-
         except HttpError as err:
             print(err)
         return [creds, service]
 
+    # ========================== Check for tabs ========================== #
+    # First step is to check to see if a tab for the given month/user exists
+        # To check for an existing tab, we need a way to read the tabs and compare against what we expect the tab for the given data should look like
+        # Tab name should take the form "date_user".  This date/use will come as a parameter when instantiating this class
     def check_for_sheet(self):
         # Call the Sheets API
-        # create access to the google sheets "services" -> get spreadsheet 
-        service = build('sheets', 'v4', credentials=self.creds)
-        working_sheet = service.spreadsheets()
-        working_sheet_values = working_sheet.values()
-
-        # ========================== Check for tabs ========================== #
-        # First step is to check to see if a tab for the given month/user exists
-            # To check for an existing tab, we need a way to read the tabs and compare against what we expect the tab for the given data should look like
-            # Tab name should take the form "date_user".  This date/use will come as a parameter when instantiating this class
-
+        working_sheet = self.service.spreadsheets()
         # Get metadata
         sheet_metadata = working_sheet.get(spreadsheetId=SPREADSHEET_ID).execute()
         # Get all sheet/tab names
@@ -99,7 +90,8 @@ class GoogleHandler(object):
             if re.search('[0-9]{4}-[0-9]+', sheet['properties']['title']):
                 self.format_sheet(sheet['properties']['sheetId'])
 
-            
+    # ========================== Create new sheet ========================== #
+    # Request body to generate a new tab/sheet given a date range and worksheet/working_sheet
     def create_new_sheet(self, working_sheet, date_range):
         body = {
             "requests" : [
@@ -108,9 +100,9 @@ class GoogleHandler(object):
                         "properties" : {
                             "title" : date_range,
                             "tabColor": {
-                                'red': 1.2,
-                                'green': 1.3,
-                                'blue': 1.4
+                                'red': 0.5,
+                                'green': 1.5,
+                                'blue': 2.5
                             }
                         }
                     },
@@ -120,23 +112,56 @@ class GoogleHandler(object):
         sheet_added_response = working_sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body).execute()        
         return sheet_added_response
 
-    def update_sheet(self, sheet_title, data_frame):
+    # ========================== Update sheet ========================== #
+    # Send the data to update a month tab/sheet
+
+    def update_sheet(self, sheet_title: str, data_frame: DataFrame) -> None:
         # use the "build"/connection to the specific "service" api and "resource" = build(serviceName, 'V4', creds=self.creds).spreadsheets.values()
         working_sheet = self.service.spreadsheets().values()
+        month = sheet_title.split('-')[1]
+        
+        # Use the given month to find the sheet/tab to update
+        sheet_to_update = self.find_sheets_given_month_user([month])[0]
+        # Use the sheet/tab to update to find where the new data starts or the "range to update"
+        sheet_to_update_with_range = self.find_update_start([sheet_to_update])[0]
+        range_to_update = f'{sheet_to_update_with_range["title"]}!F{len(sheet_to_update_with_range["values_in_column"]) + 1}:Z'
+
         # loop through a list of lists of data
         body = {
             'valueInputOption':'USER_ENTERED',
             'data': [
                 {
                     # Sheet1!A:Z -> spreadsheet info comes from the batchUpdate() arguments 
-                    "range": f'{sheet_title}!F:Z',
+                    "range": f'{range_to_update}',
                     "majorDimension": 'ROWS',
                     "values": data_frame
                 },
             ]
         }
-        # after the sheet has been updated, format it
         return working_sheet.batchUpdate(spreadsheetId=self.spreadsheet_id, body=body).execute()
+
+    def find_sheets_given_month_user(self, months_to_update: list[int] = []) -> list[str]:
+        working_sheet = self.service.spreadsheets()
+        
+        sheets_metadata = working_sheet.get(spreadsheetId=SPREADSHEET_ID).execute()
+        sheets = sheets_metadata['sheets']
+        sheets_to_update = []
+        for month in months_to_update:
+            if [sheet['properties'] for sheet in sheets if re.match(f'[0-9]+-{month}-{self.user}', sheet['properties']['title'])]:
+                sheet_to_update_metadata = [sheet['properties'] for sheet in sheets if re.match(f'[0-9]+-{month}-{self.user}', sheet['properties']['title'])][0]
+                sheets_to_update.append(sheet_to_update_metadata)
+        return sheets_to_update
+
+    def find_update_start(self, sheets_to_update: list[object]) -> list[object]:
+        working_sheet = self.service.spreadsheets()
+        column_letter = 'F'
+        for sheet in sheets_to_update:
+            values_in_column = working_sheet.values().get(
+                spreadsheetId=SPREADSHEET_ID,
+                range = f"'{sheet['title']}'!{column_letter}:{column_letter}"
+            ).execute()
+            sheet['values_in_column'] = values_in_column.get('values', [])
+        return sheets_to_update
 
     def copy_and_paste_budget_to_new_sheet(self, sheetId):
         working_sheet = self.service.spreadsheets()
